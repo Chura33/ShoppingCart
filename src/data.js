ed5a21dcdd8333f7b663b0d1a5bdd0d3f20d5c44
Chura <churabrady@gmail.com>
2022-11-25 15:39:24 -0800

    Finished shopping cart

---
+++ src/data.js
@@ -0,0 +1,25 @@
+let shopItemsData = [{
+ id:"fweohhffhbwefwn3",
+ name:"Casual Shirt",
+ price: 45,
+ desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+ img:"images/img1.jpg"
+},{
+  id:"hvjlidiiidfdffdf",
+ name:"Item 2",
+ price: 45,
+ desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+ img:"images/img2.jpg"
+},{
+  id:"hihghgijdjdjo",
+ name:"Item 3",
+ price: 45,
+ desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+ img:"images/img3.jpg"
+},{
+ id:"jgigjdjlkggbkgg",
+ name:"Item 4",
+ price: 45,
+ desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+ img:"images/img4.jpg"
+}]
